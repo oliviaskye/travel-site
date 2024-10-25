8d3759bab61e02716cd9543bf2791d4154cc154a
Minoo-Yh <minoo.yaghoubi@gmail.com>
2024-10-25 09:57:15 +0300

    Country page

---
+++ OneDrive - Vantaan Opetus/Työpöytä/React/Country/country-page/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Photo from './Components/Photo';
+import tailwindConfig from '../tailwind.config';
+
+import './App.css'
+
+
+  function App() {
+    return (
+      <div className="App">
+        <Photo />
+      </div>
+    );
+  }
+export default App;
---
+++ OneDrive - Vantaan Opetus/Työpöytä/React/Country/country-page/src/Components/Photo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Photo = () => {
+  return (
+    <div>
+      <img src='https://plus.unsplash.com/premium_photo-1677343210638-5d3ce6ddbf85?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+    </div>
+  )
+}
+
+export default Photo;
