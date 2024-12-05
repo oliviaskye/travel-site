12973a305dd78e111d02c5be1e94afabf2ad0a78
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2024-12-05 13:25:47 +0200

    fix small thinges

--- api/routes/Auth.js
+++ api/routes/Auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   register,
   login,
-  getUsers,
+  GetUsers,
   getUser,
   deleteUser,
   PutUser,
@@ -14,13 +14,8 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 
-<<<<<<< HEAD
 router.get("/users", GetUsers);
 router.get("/users/:id", getUser);
-=======
-router.get("/users", verifyToken, isAdmin, getUsers);
-router.get("/users/:id", verifyToken, isAdmin, getUser);
->>>>>>> ac5b9dec60074e4ce6223173ed67d4042f0c37cb
 
 router.delete("/users/:id",  deleteUser);
 
