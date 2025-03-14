6a526c43155f7efe0e3c52e642fdaad8734fa2b6
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2025-03-14 09:24:36 +0200

    fix then iamge sin hotel and rooms

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,8 @@ export default defineConfig({
     alias: {
       "@assets": path.resolve(__dirname, "public/assets/images"),
       "@Nav": path.resolve(__dirname, "src/components/Nav/Nav.jsx"),
+      "@container": path.resolve(__dirname, "src/Style/container.css"),
+      "@Index": path.resolve(__dirname, "src/index.css"),
     },
   },
 });
