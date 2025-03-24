64118528b5dc91450e62c1030b501fbdd22ef210
Minoo-Yh <minoo.yaghoubi@gmail.com>
2025-03-24 18:30:01 +0200

    commit changes in travel-site submodule

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,43 +1,17 @@
 import React from "react";
-import footerCSS from "./Footer.module.css";
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer className={`${footerCSS.footer_wrapper} section`}>
-      <div className={footerCSS.FooterLinks}>
-        <div className={footerCSS.logo}>
-
-     
-        </div>
-      </div>
-
-
-
-      <div className={footerCSS.FooterLinks}>
-        <h3>Quick Links</h3>
-        <p>About</p>
-        <p>Contact Us</p>
-        <p>Rooms</p>
-        <p>GYM</p>
-        <p>Restaurant</p>
-      </div>
-
-      <div className={footerCSS.FooterLinks}>
-        <h3>City Branches</h3>
-        <p>Heslinki</p>
-        <p>Vantaa</p>
-        <p>Espooo</p>
-        <p>Lapii</p>
-        <p>Turku</p>
-      </div>
-
-      <div className={footerCSS.FooterLinks}>
-        <h3>More Info</h3>
-        <p>Terms of Service</p>
-        <p>Privacy Policy</p>
-      </div>
+    <footer className="footer">
+      <p>&copy; 2025 Traveller. All Rights Reserved.</p>
+      <Link to="/contact" className="contact-link">
+        Contact Us
+      </Link>
     </footer>
   );
 };
 
 export default Footer;
+
